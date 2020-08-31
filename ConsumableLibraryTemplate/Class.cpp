6e376136aa9ce8c8f6e740cd601fb0fdc3361be6
twilker <twilker@phoenixcontact.com>
2020-08-31 13:11:58 +0200

    Add template for consumable library #exported-commit:3eb9f2303cda7f0ca05a687e4727107334b52cce

---
+++ ConsumableLibraryTemplate/Class.cpp
@@ -0,0 +1,5 @@
+﻿#include "$(name)/$(template.files.class.name)"
+
+$(namespace.format.start)
+
+$(namespace.format.end) // end of namespace $(namespace)
---
+++ ConsumableLibraryTemplate/Class.hpp
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include "Arp/System/Core/Arp.h"
+
+$(namespace.format.start)
+
+using namespace Arp;
+
+class $(name)Class
+{
+public: // typedefs
+    typedef std::shared_ptr<$(name)Class> Ptr;
+
+public: // construction/destruction
+    /// <summary>Constructs an <see cref="$(name)Class" /> instance.</summary>
+    $(name)Class(void) = default;
+    /// <summary>Copy constructor.</summary>
+    $(name)Class(const $(name)Class& arg) = default;
+    /// <summary>Assignment operator.</summary>
+    $(name)Class& operator=(const $(name)Class& arg) = default;
+    /// <summary>Destructs this instance and frees all resources.</summary>
+    ~$(name)Class(void) = default;
+
+public: // operators
+
+public: // static operations
+
+public: // setter/getter operations
+
+public: // operations
+
+protected: // operations
+
+private: // static methods
+
+private: // methods
+
+public: // fields
+
+private: // static fields
+
+};
+
+$(namespace.format.end) // end of namespace $(namespace)
