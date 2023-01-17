578548b95af8a6a9e1d23e87da5bd442924327b4
pxcsw-afbuild <67624542+pxcsw-afbuild@users.noreply.github.com>
2023-01-17 08:33:56 +0100

    Template native shared #exported-commit:cf31b906b762c7a0637297726e52277bf5f224b4

---
+++ SharedNativeFunctionTemplate/NativeFunction.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Copyright (c) Phoenix Contact GmbH & Co KG
+//  This software is licensed under Apache-2.0
+//
+///////////////////////////////////////////////////////////////////////////////
+#endregion
+
+using System;
+using System.Iec61131Lib;
+using System.Runtime.InteropServices;
+using Eclr;
+using Iec61131.Engineering.Prototypes.Types;
+using Iec61131.Engineering.Prototypes.Variables;
+using Iec61131.Engineering.Prototypes.Common;
+using Iec61131.Engineering.Prototypes.Methods;
+using Iec61131.Engineering.Prototypes.Pragmas;
+
+namespace $(namespace)
+{
+    [Native]
+    [Function$(returntype.toupper.format.datatypeattribute)]
+    public static class $(name)
+    {
+        [Execution]
+        public static $([if]$(returntype.format.isforcedreturnbyref)||$(returnbyref))void$([else])$(returntype.format.csharpdatatype)$([end-if]) __Process(
+            $([if]$(returntype.format.isforcedreturnbyref)||$(returnbyref))[Output] ref $(returntype.format.csharpdatatype) $(name),
+            $([end-if])[Input] short IN1,
+            [Input, DataType("WORD")] ushort IN2)
+        {
+            // No implementation in C# part; implement in native method
+$([if]$(returntype.format.isforcedreturnbyref.negate)&&$(returnbyref.negate))
+            // Just return something to avoid errors from the C# compiler.
+            return $(returntype.format.csharpdatatype.format.initvalue);
+$([end-if])
+        }
+    }
+}
---
+++ SharedNativeProjectTemplate/Cpp/.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#ifndef PLATFORM_DATAMODEL_P64_DEFINED
+  #include "$(name)-cli32.h"
+#else
+  #include "$(name)-cli64.h"
+#endif
