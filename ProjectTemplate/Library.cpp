46a6548beed1eef00a550e47306095ffca02d787
pxcsw-afbuild <67624542+pxcsw-afbuild@users.noreply.github.com>
2024-10-28 11:30:40 +0100

    use escaped project name also in plm project; #exported-commit:aaabaef4e4866382c49fadf1e6124cd6bb8de7e7

--- ProjectTemplate/Library.cpp
+++ ProjectTemplate/Library.cpp
@@ -11,6 +11,7 @@ $([foreach]component[in]related[of-type]basecomponent)
 $([end-foreach])
 
 $(namespace.format.start)
+
 #if ARP_ABI_VERSION_MAJOR < 2
 $(name.format.lastNamespacePart.format.escapeProjectName)Library::$(name.format.lastNamespacePart.format.escapeProjectName)Library(AppDomain& appDomain)
     : MetaLibraryBase(appDomain, ARP_VERSION_CURRENT, typeDomain)
@@ -46,17 +47,17 @@ $(name.format.lastNamespacePart.format.escapeProjectName)Library& $(name.format.
 }
 #endif
 
+
 #if ARP_ABI_VERSION_MAJOR < 2
 extern "C" ARP_CXX_SYMBOL_EXPORT ILibrary& ArpDynamicLibraryMain(AppDomain& appDomain)
 {
     $(name.format.lastNamespacePart.format.escapeProjectName)Library::Main(appDomain);
     return  $(name.format.lastNamespacePart.format.escapeProjectName)Library::GetInstance();
 }
-
 $(namespace.format.end) // end of namespace $(namespace)
 #else
 $(namespace.format.end) // end of namespace $(namespace)
-extern "C" ARP_EXPORT Arp::Base::Acf::Commons::ILibrary& $(name)_MainEntry()
+extern "C" ARP_EXPORT Arp::Base::Acf::Commons::ILibrary& $(name.format.escapeDotProjectName)_MainEntry()
 {
     return  $(namespace.format.cppFullName)::$(name.format.lastNamespacePart.format.escapeProjectName)Library::GetInstance();
 }
