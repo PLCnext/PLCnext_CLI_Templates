2b4a4e5e998589a6b69b29cf614e8fe2d52ac2f8
pxcsw-afbuild <67624542+pxcsw-afbuild@users.noreply.github.com>
2025-09-19 09:42:30 +0200

    move test template to unit tests; #exported-commit:7d31b45a7a12a233be71726c55f4225ef46d018c

--- TestTemplate/LibraryInfo.hpp
+++
@@ -1,8 +0,0 @@
-#include "Arp/Base/Core/Arp.hpp"
-
-namespace $(namespace.format.cppFullName)
-{
-
-	const ArpVersion $(name.format.lastNamespacePart.format.escapeProjectName)LibraryVersion = ArpVersion(1); // adjust your library version number here
-
-} // end of namespace $(namespace)
