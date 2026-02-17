fbcbad0d077265ab9d477eaa631dd4dff89cf8df
pxcsw-afbuild <67624542+pxcsw-afbuild@users.noreply.github.com>
2026-02-17 07:48:08 +0100

    add call to SetSdkBuildInfo in constuctor for ARP version >= 2.1; #exported-commit:ccd7d7a7d1b40c7395a6ec23f50bee64b5ab4cb0

--- FieldbusProjectTemplate/Library.cpp
+++ FieldbusProjectTemplate/Library.cpp
@@ -5,18 +5,18 @@
 ///////////////////////////////////////////////////////////////////////////////
 #include "$(template.files.library.format.include)"
 
-//#include "FieldbusComponent.hpp"
-
 $([foreach]component[in]related[of-type]basecomponent)
 #include "$(component.template.files.component.format.include)"
 $([end-foreach])
-
 
 $(namespace.format.start)
 
 $(name.format.lastNamespacePart.format.escapeProjectName)Library::$(name.format.lastNamespacePart.format.escapeProjectName)Library()
     : IoLibraryBase(ARP_VERSION_BUILT)
     {
+#if ARP_ABI_VERSION_MAJOR >= 3 || (ARP_ABI_VERSION_MAJOR >= 2 && ARP_ABI_VERSION_MINOR >= 1)
+    this->SetSdkBuildInfo(ARP_VERSION_BUILT, ARP_ABI_VERSION_NAME, ARP_TARGET_IDENTIFIER);
+#endif
 $([foreach]component[in]related[of-type]basecomponent) 
         this->AddComponentType<::$(component.fullName)>();
 $([end-foreach])
